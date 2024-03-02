2294c4d03c5be922a735e8b99663ba0f54e57034
MaccBass <oxq1234@dongguk.edu>
2024-03-02 14:04:40 +0900

    feat: 이동 시 sprite 변경

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,24 +8,39 @@ public class PlayerController : MonoBehaviour
     // Private 변수인 speed를 inspector에 노출
     [SerializeField]
     float speed = 10f;
+    public Sprite[] sprites = new Sprite[4];
 
+    SpriteRenderer sr;
     void Start()
     {
         // 이미 액션이 할당되어 있을 경우 삭제 후 다시 추가
         Manager.Input.KeyAction -= ControllByWASD;
         Manager.Input.KeyAction += ControllByWASD;
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // WASD 키를 통한 플레이어 컨트롤
     void ControllByWASD()
     {
         if (Input.GetKey(KeyCode.W))
+        {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
+            sr.sprite = sprites[0];
+        }
         if (Input.GetKey(KeyCode.S))
+        {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
+            sr.sprite = sprites[1];
+        }
         if (Input.GetKey(KeyCode.A))
+        {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
+            sr.sprite = sprites[2];
+        }
         if (Input.GetKey(KeyCode.D))
+        {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
+            sr.sprite = sprites[3];
+        }
     }
 }
