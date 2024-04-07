97d222629253b2b09613b08b990f8ef525bc087b
Hyunwoo Moon <113423186+m00nhyunw00@users.noreply.github.com>
2024-04-07 20:06:09 +0900

    Feat: 게임 오버 화면 페이드 아웃 구현

--- Assets/Scripts/UI/MovementObject.cs
+++ Assets/Scripts/UI/MovementObject.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class MovementObject : MonoBehaviour
 {
@@ -18,11 +16,18 @@ public class MovementObject : MonoBehaviour
     private float movespeed = 10;
     private float animspeed = 1;
     Animator anim;
-    private void Start() {
+
+    private bool canMove = true; // 플레이어가 움직일 수 있는지 여부를 결정하는 변수
+
+    void Start()
+    {
         // 저장된 위치 데이터불러오기
-        if(Manager.Data.getIsLoad()) {
+        if (Manager.Data.getIsLoad())
+        {
             transform.position = new Vector3(Manager.Data.nowPlayer.PlayerPosX, Manager.Data.nowPlayer.PlayerPosY, 0);
-        } else {
+        }
+        else
+        {
             Debug.Log("Load fail");
         }
         anim = GetComponent<Animator>();
@@ -30,42 +35,46 @@ public class MovementObject : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float x = virtualJoystick.Horizontal(); // �� / ��
-        float y = virtualJoystick.Vertical();   // �� / �Ʒ�
-        
-#if UNITY_EDITOR
-        if (Input.GetKey(KeyCode.W))
-            y = 1f;
-        if (Input.GetKey(KeyCode.S))
-            y = -1f;
-        if (Input.GetKey(KeyCode.A))
-            x = -1f;
-        if (Input.GetKey(KeyCode.D))
-            x = 1f;
-#endif
- 
-        if (x!=0 || y!=0) {
-            PlayerMove(new Vector2(x, y));
-        }
-        else
+        if (canMove)
         {
-            PlayerIdle();
+            float x = virtualJoystick.Horizontal(); // 왼쪽 / 오른쪽
+            float y = virtualJoystick.Vertical();   // 아래 / 위
+
+            // 키보드 입력을 통한 움직임 추가
+#if UNITY_EDITOR
+            if (Input.GetKey(KeyCode.W))
+                y = 1f;
+            if (Input.GetKey(KeyCode.S))
+                y = -1f;
+            if (Input.GetKey(KeyCode.A))
+                x = -1f;
+            if (Input.GetKey(KeyCode.D))
+                x = 1f;
+#endif
+
+            if (x != 0 || y != 0)
+            {
+                PlayerMove(new Vector2(x, y));
+            }
+            else
+            {
+                PlayerIdle();
+            }
         }
     }
 
-    void PlayerMove(Vector2 direction) {
-
+    void PlayerMove(Vector2 direction)
+    {
         transform.Translate(direction * movespeed * Time.fixedDeltaTime);
         animspeed = Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.y, 2));
         anim.SetFloat("animspeed", animspeed);
 
-        // transform.eulerAngles
         float dir = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
         GetDirection(dir);
 
         anim.Play($"Walk_{_state.ToString()}");
-        
     }
+
     void GetDirection(float dir)
     {
         if (dir <= 45.0 && dir >= -45.0) _state = PlayerDirection.Up;
@@ -73,10 +82,16 @@ public class MovementObject : MonoBehaviour
         else if (dir <= -45.0 && dir >= -135.0) _state = PlayerDirection.Left;
         else _state = PlayerDirection.Down;
     }
+
     void PlayerIdle()
     {
         anim.SetBool("isMoving", false);
         anim.Play($"Idle_{_state.ToString()}");
     }
 
+    // 플레이어가 움직일 수 있는지 여부를 설정하는 메소드
+    public void SetCanMove(bool value)
+    {
+        canMove = value;
+    }
 }
